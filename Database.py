4661eb9ecc0c07befbaa83f26494e472beb83083
Dhruv Kunjumon <dhruvsci30@gmail.com>
2017-03-31 22:24:02 +0530

    Don't know what i did :(.

--- Database.py
+++ Database.py
@@ -12,56 +12,56 @@ def get_time():
 
 def create_database():
     c.execute("""CREATE TABLE IF NOT EXISTS encrypts_storage(
-    website TEXT NOT NULL,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL,
+    web TEXT NOT NULL,
+    usr TEXT NOT NULL,
+    pswd TEXT NOT NULL,
     create_date TEXT,
     last_used TEXT,
-    PRIMARY KEY (website, username))""")
+    PRIMARY KEY (web, usr))""")
     return
 
 
-def create_account(website, username, password):
+def create_account(web, usr, pswd):
     curr_date = get_time()
     c.execute("""INSERT INTO encrypts_storage
-    (website, username, password, create_date, last_used)
+    (web, usr, pswd, create_date, last_used)
     VALUES (?, ?, ?, ?, ?)""",
-              (website, username, password, curr_date, curr_date))
+              (web, usr, pswd, curr_date, curr_date))
     conn.commit()
     return
 
 
-def search_account(website, username):
+def search_account(web, usr):
     c.execute("""SELECT *
     FROM encrypts_storage
-    WHERE website=? AND username=?""",
-              (website, username))
+    WHERE web=? AND usr=?""",
+              (web, usr))
     data = c.fetchall()
     attrs = ["website", "username", "password", "last_used", "create_date"]
     data = dict(zip(attrs, data[0])) if data else None
     return data
 
 
-def delete_account(website, username):
+def delete_account(web, usr):
     c.execute("""DELETE FROM encrypts_storage
-    WHERE website=? AND username=?""",
-              (website, username))
+    WHERE web=? AND usr=?""",
+              (web, usr))
     conn.commit()
     return
 
 
-def update_account(website, username, new_username, new_password):
+def update_account(web, usr, new_usr, new_pswd):
     curr_date = get_time()
     c.execute("""UPDATE encrypts_storage
-    SET username=?, password=?, last_used=?
-    WHERE website=? AND username=?""",
-              (new_username, new_password, curr_date, website, username))
+    SET usr=?, pswd=?, last_used=?
+    WHERE web=? AND usr=?""",
+              (new_usr, new_pswd, curr_date, web, usr))
     conn.commit()
     return
 
 
 def show_all():
-    c.execute("""SELECT website, username, create_date, last_used
+    c.execute("""SELECT web, usr, create_date, last_used
     FROM encrypts_storage""")
     data = c.fetchall()
     return data
